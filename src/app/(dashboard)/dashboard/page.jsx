c23c70ef9bed3eeb9eafbd398a598339d7f122bd
Chibuzo Ekwue <chieezy5@gmail.com>
2023-11-29 16:22:44 +0100

    Dashboard page

---
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const DashboardPage = () => {
+	return (
+		<div className={styles.dashboard}>
+			<div className="container"></div>
+		</div>
+	);
+};
+
+export default DashboardPage;
